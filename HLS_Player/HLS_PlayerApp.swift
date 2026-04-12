11d464e7d9ece4ce675327e106bdfd3e857f61da
Zezinas <108526834+Zezinas@users.noreply.github.com>
2026-04-12 16:13:22 +0300

    Initial project setup

---
+++ HLS_Player/HLS_PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HLS_PlayerApp.swift
+//  HLS_Player
+//
+//  Created by Zezinas on 2026-04-12.
+//
+
+import SwiftUI
+
+@main
+struct HLS_PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
